8ef9f6a6f54556efa39947a69cc5d20cc8b08abc
JuanBerrocal <juan.brudner@gmail.com>
2026-02-04 06:26:42 +0100

    Swagger added to the project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const testRoutes = require('./routes/test.routes');
 const usersRoutes = require('./routes/users.routes');
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require('./swagger')
+
 const app = express()
 const PORT = 3000
 
-app.use(express.json())
+app.use(express.json());
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api', testRoutes);
 app.use('/api/users', usersRoutes);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,28 @@ const express = require("express");
 const router = express.Router();
 const {getUsers} = require('../controllers/users.controller');
 
+/**
+ * @openapi
+ * /api/users/getusers:
+ *   get:
+ *     summary: Obtiene todos los usuarios
+ *     tags:
+ *       - Users
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                      type: object
+ */
 router.get('/getusers', getUsers);
 
 module.exports = router;
